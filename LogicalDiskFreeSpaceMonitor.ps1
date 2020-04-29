b0ff0ef9d1679698b6817b09a77a32d1f05a0697
Yannick Zwijsen <zwijse@gmail.com>
2020-04-30 01:00:19 +0200

    cleanup

--- LogicalDiskFreeSpaceMonitor.ps1
+++ LogicalDiskFreeSpaceMonitor.ps1
@@ -12,9 +12,6 @@ $Drive = Get-PSDrive $DriveLetter
 $DriveFreeBytes = $Drive | Select-Object -ExpandProperty Free
 $DriveUsedBytes = $Drive | Select-Object -ExpandProperty Used
 
-#We could use wmi as well
-#$Drive = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='$($TargetDrive):'" | Select-Object FreeSpace, Size
-
 #Calculate Additional Data
 $DriveTotalBytes = $DriveUsedBytes + $DriveFreeBytes
 $DriveFreePercent = $DriveFreeBytes / $DriveTotalBytes * 100
@@ -29,7 +26,7 @@ else
 	$PropertyBag.AddValue("State","Healthy") 
 }
 
-#Calculate Mb values for SCOM Alert
+#Calculate Mb values so we can use that data in the SCOM Alert
 $DriveFreeMegaBytes = $DriveFreeBytes * 1024 * 1024
 $DriveUsedMegaBytes = $DriveUsedBytes * 1024 * 1024
 $DriveTotalMegaBytes = $DriveTotalBytes * 1024 * 1024
